6b316c756285aa73fee10ca88830e8782cdfc9d6
Тимур <ladnoladno477@gmail.com>
2024-12-17 20:45:46 +0500

    task 17.3

--- app/models/task.py
+++ app/models/task.py
@@ -1,4 +1,4 @@
-from backend.db import Base
+from app.backend.db import Base
 from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -9,6 +9,10 @@ class Task(Base):
     content = Column(String)
     priority = Column(Integer, default=0)
     completed = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     slug = Column(String, unique=True, index=True)
     user = relationship('User', back_populates='task')
+
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(Task.__table__))
--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from backend.db import Base
+from app.backend.db import Base
 from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 
